36f48b937a08b92ce7afc1ca68aa5ccccad41f82
marcie <masashi.shimano0409@gmail.com>
2024-03-07 04:11:51 +0000

    日付別売上 テーブル表示

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -6,6 +6,8 @@ import { getToday } from '@/common'
 
 const form = reactive({ startDate: null, endDate: null, type: 'perDay' })
 
+const data = reactive({})
+
 onMounted(() => {
     form.startDate = getToday()
     form.endDate = getToday()
@@ -21,7 +23,7 @@ const getData = async () => {
             }
         })
         .then( res => {
-            // data.value = res.data
+            data.data = res.data.data
             console.log(res.data)
         })
     } catch (e){
@@ -48,6 +50,23 @@ const getData = async () => {
                      To: <input type="date" name="endDate" v-model="form.endDate"><br>
                      <button>分析する</button>
                     </form>
+                    
+                    <div v-if="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
+                        <table class="table-auto w-full text-left whitespace-no-wrap">
+                            <thead>
+                                <tr>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">年月日</th>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">金額</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="item in data.data" :key="item.date">
+                                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.date }}</td>
+                                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.total }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
